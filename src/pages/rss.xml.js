c22ff1b926ece15828aeda7d447f2f5d98679a06
c92d58 <x@wahsun.org>
2026-05-31 03:21:57 -0400

    fix: update URLs to blog.wahsun.org in BaseHead and RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,18 +1,11 @@
-import rss from "@astrojs/rss";
-import { getCollection } from "astro:content";
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
-export async function GET() {
-  const posts = await getCollection('posts');
-  return rss({
-    title: 'WAHSUN | Blog',
-    description: 'WAHSUN 的个人博客',
-    site: 'https://c92d58.github.io/blog',
-    items: posts.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      link: `/blog/posts/${post.id}/`,
-    })),
-    customData: `<language>zh-TW</language>`,
-  });
+export async function GET(context) {
+    return rss({
+        title: 'WAHSUN Blog',
+        description: 'WAHSUN 的个人博客',
+        site: 'https://blog.wahsun.org',
+        items: await pagesGlobToRssItems(import.meta.glob('./posts/*.md')),
+        customData: `<language>zh-cn</language>`,
+    });
 }
